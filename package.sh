45c4be4210fc51fc3ce4db23f4c63d75ee292ece
beazlr02 <ross.beazley@bbc.co.uk>
2020-11-24 13:29:10 +0000

    segment

--- package.sh
+++ package.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-ffmpeg -f avfoundation -framerate 30 -video_size 640x480 -i "0:none" -vcodec libx264 -preset ultrafast -tune zerolatency -pix_fmt yuv420p -map 0:v -profile:v:0 main -f dash -min_seg_duration 4000 -use_template 1 -use_timeline 0 -remove_at_exit 0 cam.mpd
+ffmpeg -f avfoundation -framerate 30 -video_size 640x480 -i "0:none" -vcodec libx264 -preset ultrafast -tune zerolatency -pix_fmt yuv420p -map 0:v -profile:v:0 main -f dash -seg_duration 10 -use_template 1 -use_timeline 0 -remove_at_exit 0 -window_size 100 cam.mpd
